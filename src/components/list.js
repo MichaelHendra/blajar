88893cc641289f37652663a3e5c24698d0f44660
MichaelHendra <michaelhendranata01@gmail.com>
2024-06-25 07:05:21 +0700

    4.Props

---
+++ src/components/list.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function List(props){
+return(
+    <div>
+        <h1> {props.listData} </h1>
+    </div>
+);
+}
+export default List;
---
+++ src/props.js
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./components/list";
+
+function Props (){
+    return(
+        <div>
+            <List listData ="XD" />
+            <List listData ="XD" />
+            <List listData ="XD" />
+        </div>
+    );
+    }
+
+export default Props;
